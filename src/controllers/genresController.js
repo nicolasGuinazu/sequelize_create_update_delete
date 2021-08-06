ce8ac9aad97174112d96242d6ee8e3f6144b5bb9
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-06 19:29:50 -0300

    commented lines to work on them later

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -3,25 +3,12 @@ const sequelize = db.sequelize;
 
 
 const genresController = {
-    'list': (req, res) => {
+    list: (req, res) => {
 
-        Modelo
-        db.Genre.findAll()
-            // Salio bien la promesa
-            .then(genres => {
-                res.render('genresList.ejs', {genres})
-            })
-            // Salio mal
-            .catch(error => {
-                return res.send('Ocurrió un error')
-            })
             
     },
-    'detail': (req, res) => {
-        db.Genre.findByPk(req.params.id)
-            .then(genre => {
-                res.render('genresDetail.ejs', {genre});
-            });
+    detail: (req, res) => {
+
     }
 
 }
