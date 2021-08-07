247fc523de36ec7540c7dd4cd43bea75fc5261dc
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-07 11:55:45 -0300

    list and details methods

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -1,15 +1,26 @@
 const db = require('../database/models');
 const sequelize = db.sequelize;
-
+const Genre = db.Genre;
 
 const genresController = {
-    list: (req, res) => {
-
+    list: async (req, res) => {
+        let genreList=[]
+        try{
+            genreList=await Genre.findAll();
+            return res.render('genresList',{genreList})
+        }catch(err){
+            console.log(err)
+        }
             
     },
-    detail: (req, res) => {
-
-    }
+    detail: async (req, res) => {
+        try{
+            genre=await Genre.findByPk(req.params.id);
+            return res.render('genresDetail',{genre})
+        }catch(err){
+            console.log(err)
+        }
+    },
 
 }
 
