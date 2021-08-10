8ce0f4f4a6f7aea8be9db811919f3e4d461cb79e
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-10 17:58:53 -0300

    to test actors i used index, Ill create an actors route and controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../database/models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Digital Movies' });
+});
+
+router.get('/actor/:id', async(req, res, next)=>{
+  let actor=await db.Actor.findByPk(req.params.id,{
+    include:['movies']
+  })
+    
+   return res.render('actorDetail',{actor});  
 });
 
 module.exports = router;
