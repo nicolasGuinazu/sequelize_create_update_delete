aac8f02ac707b464142bf5f62b27d9b775aac4b3
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-10 17:57:49 -0300

    added many to many relationshhips

--- src/database/models/Movie.js
+++ src/database/models/Movie.js
@@ -32,6 +32,13 @@ module.exports = (sequelize, dataTypes) => {
         Movie.belongsTo(models.Genre,{
             as:'genre',
             foreignKey:'genre_id'
+        }),
+        Movie.belongsToMany(models.Actor,{
+            as:'actors',
+            through:'actor_movie',
+            foreignKey:'movie_id',
+            otherKey:'actor_id',
+            timestamps: false
         })
     }
     
