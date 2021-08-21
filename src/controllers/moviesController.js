2d263a234f644df4482a92296bc42e72459b1bd8
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-08-21 10:47:59 -0300

    logic to filter searched movies and if not found api call to search

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,10 +1,39 @@
 const db = require('../database/models');
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
+const fetch=require('node-fetch')
 //Otra forma de llamar a los modelos
 const Movie = db.Movie;
 
 const moviesController = {
+    search:async(req,res)=>{
+       
+        try{
+           let movie=await Movie.findOne({
+               include:["genre","actors"],
+                where:{
+                    title: {
+                        [Op.like]: `%${req.query.search}%`
+                      }
+                },
+            });
+            if (movie){
+                let movieLi=await Movie.findAll();
+                let movieList= await movieLi.filter((el)=>el.title==movie.title)
+                return res.render('moviesList',{movieList})
+            }else{
+            
+                fetch(`http://www.omdbapi.com/?apikey=d4e35e92&s=${req.query.search}`)
+                .then(res=>res.json())
+                .then(data=>res.send(movieList=data["Search"]))
+
+            }
+            
+           
+        }catch(err){
+            console.log(err)
+        }
+    },
     list: async (req, res) => {
         let movieList=[]
         try{
@@ -17,7 +46,7 @@ const moviesController = {
     },
     detail: async (req, res) => {
         try{
-            movie=await Movie.findByPk(req.params.id,{
+            let movie=await Movie.findByPk(req.params.id,{
                 include:["genre","actors"]
             });
             
